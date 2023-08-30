86afdf23d84c88d58e9e591a3f6cdb7caff19492
chengwei.fu <1572060320@qq.com>
2023-08-30 16:13:54 +0800

    fix: 修复低版本不支持可选链语法问题

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
+    target: 'es2015',
     lib: {
       entry: resolve(__dirname, 'src/index.js'),
       formats: ['es', 'umd'],
